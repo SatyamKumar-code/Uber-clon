9d5d89e49e36b958012b343630e9f1af397fb5e9
SatyamKumar-code <satyamwebsite1@gmail.com>
2025-01-07 00:22:34 +0530

    first commit

---
+++ frontend/src/components/FinishRide.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+const FinishRide = (props) => {
+    const navigate = useNavigate()
+
+    async function endRide() {
+        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/rides/end-ride`, {
+             
+            rideId: props.ride._id
+            
+        },{
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+        
+
+        if (response.status === 200) {
+            navigate('/captain-home')
+        }
+    }
+
+    return (
+        <div>
+            <h5 className='p-1 text-center w-[93%] absolute top-0' onClick={() => {
+                props.setFinishRidePanel(false)
+            }}><i className='text-3xl text-gray-200 ri-arrow-down-wide-line'></i></h5>
+            <h3 className='text-2xl font-semibold mb-5'>Finish this ride</h3>
+            <div className='flex items-center justify-between p-4 border-2 border-yellow-400 rounded-lg mt-4'>
+                <div className='flex items-center gap-3'>
+                    <img className='h-14 w-14 rounded-full object-cover' src="https://preview.redd.it/created-random-people-using-chatgpt-midjourney-do-you-know-v0-q1aa450i5dqb1.png?width=1024&format=png&auto=webp&s=c4e9abc47d193474a2fa1a7e337d9d9340dce947" alt="" />
+                    <h2 className='text-lg font-medium capitalize'>{props.ride?.user.fullname.firstname + " " + props.ride?.user.fullname.lastname}</h2>
+                </div>
+                <h5 className='text-lg font-semibold'>2.2KM</h5>
+            </div>
+            <div className='flex gap-2 justify-between flex-col items-center'>
+                <div className='w-full mt-5'>
+                    <div className='flex items-center gap-5 p-3 border-b-2'>
+                        <i className='text-lg ri-map-pin-user-fill'></i>
+                        <div>
+                            <h3 className='text-lg font-medium'>562/11-A</h3>
+                            <p className='text-sm -mt-1 text-gray-600'>{props.ride?.pickup}</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-5 p-3 border-b-2'>
+                        <i className='text-lg ri-map-pin-2-fill'></i>
+                        <div>
+                            <h3 className='text-lg font-medium'>562/11-A</h3>
+                            <p className='text-sm -mt-1 text-gray-600'>{props.ride?.destination}</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-5 p-3'>
+                        <i className='text-lg ri-currency-line'></i>
+                        <div>
+                            <h3 className='text-lg font-medium'>₹{props.ride?.fare}</h3>
+                            <p className='text-sm -mt-1 text-gray-600'>Case Case</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className='mt-10 w-full'>
+                    
+                    <button 
+                        onClick={endRide}
+                    className='w-full mt-5 flex text-lg justify-center bg-green-600 text-white font-semibold p-3 rounded-lg'>Finish Ride</button>
+
+                    <p className='mt-2 text-xs'>Click on finsh ride button if you have completed the payment.</p>
+                        
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FinishRide
---
+++ frontend/src/components/RidePopUp.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const RidePopUp = (props) => {
+    return (
+        <div>
+            <h5 className='p-1 text-center w-[93%] absolute top-0' onClick={() => {
+                props.setRidePopupPanel(false)
+            }}><i className='text-3xl text-gray-200 ri-arrow-down-wide-line'></i></h5>
+            <h3 className='text-2xl font-semibold mb-5'>New Ride Available!</h3>
+            <div className='flex items-center justify-between p-3 bg-yellow-400 rounded-lg mt-4'>
+                <div className='flex items-center gap-3'>
+                    <img className='h-14 w-14 rounded-full object-cover' src="https://preview.redd.it/created-random-people-using-chatgpt-midjourney-do-you-know-v0-q1aa450i5dqb1.png?width=1024&format=png&auto=webp&s=c4e9abc47d193474a2fa1a7e337d9d9340dce947" alt="" />
+                    <h2 className='text-lg font-medium'>{props.ride?.user.fullname.firstname + " " + props.ride?.user.fullname.lastname}</h2>
+                </div>
+                <h5 className='text-lg font-semibold'>2.2KM</h5>
+            </div>
+            <div className='flex gap-2 justify-between flex-col items-center'>
+                <div className='w-full mt-5'>
+                    <div className='flex items-center gap-5 p-3 border-b-2'>
+                        <i className='text-lg ri-map-pin-user-fill'></i>
+                        <div>
+                            <h3 className='text-lg font-medium'>562/11-A</h3>
+                            <p className='text-sm -mt-1 text-gray-600'>{props.ride?.pickup}</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-5 p-3 border-b-2'>
+                        <i className='text-lg ri-map-pin-2-fill'></i>
+                        <div>
+                            <h3 className='text-lg font-medium'>562/11-A</h3>
+                            <p className='text-sm -mt-1 text-gray-600'>{props.ride?.destination}</p>
+                        </div>
+                    </div>
+                    <div className='flex items-center gap-5 p-3'>
+                        <i className='text-lg ri-currency-line'></i>
+                        <div>
+                            <h3 className='text-lg font-medium'>₹{props.ride?.fare}</h3>
+                            <p className='text-sm -mt-1 text-gray-600'>Case Case</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className=' w-full mt-5 flex items-center justify-evenly'>
+                <button onClick={() => {
+                        props.setRidePopupPanel(false)
+
+                    }} className=' bg-gray-300 text-gray-700 font-semibold p-3 px-10 rounded-lg'>Ignore</button>
+                    <button onClick={() => {
+                        props.setConfirnRidePopupPanel(true)
+                        props.confirmRide()
+
+                    }} className=' bg-green-600 text-white font-semibold p-3 px-10 rounded-lg'>Accept</button>
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RidePopUp;
