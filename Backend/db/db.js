c57fd01bd26fb0d790fe04e4a20f3c0a6b4f50eb
Satyam Kumar <23tec2cs080@vgu.ac.in>
2025-01-10 15:21:01 +0530

    Update db.js

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 function connectToDb() {
-    mongoose.connect(process.env.DB_CONNECT
+    mongoose.connect('mongodb+srv://satyamwebsite1:04cNNIRMjY0ylRzT@cluster0.ybvid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     ).then(() => {
         console.log("Connect to DB");
     }).catch(err => console.log(err));
